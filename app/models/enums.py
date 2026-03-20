15dd8136a4d9edb012f8669d3b74cf6d6e3bf470
jogit25 <johan26005@gmail.com>
2026-03-20 19:01:06 +0530

    fix circular import by removing router inclusion from models

--- app/models/enums.py
+++ app/models/enums.py
@@ -1,7 +1,6 @@
 from enum import Enum
 
-class sourceEnum(str,Enum):
+class SourceEnum(str,Enum):
     clinic_emr="clinic_emr"
     hospital_discharge="hospital_discharge"
     patient_reported="patient_reported"
-    
